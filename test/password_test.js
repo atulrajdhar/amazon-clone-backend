e6430bc85eef2d441e1f4bf59aa4d78d5e5716c0
Atul Rajdhar <atulrajdhar@gmail.com>
2020-11-04 00:03:29 +0530

    username password validated with mocha

--- test/password_test.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect;
-
-describe('checkForPassword', function () {
-    const validatePassword = require('../helpers').validatePassword;
-    it('should contain at least 8 characters', function () {
-        
-    });
-});
